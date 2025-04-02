e28f24d78ed09e71d566172b10887e81a4f5807d
PedroMolon <pedroh.molonr@gmail.com>
2025-04-02 14:14:14 -0300

    Criação da entidade User

---
+++ src/main/java/com/pedromolon/ToDo/entity/User.java
@@ -0,0 +1,63 @@
+package com.pedromolon.ToDo.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, List<Task> tasks) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.tasks = tasks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+}
