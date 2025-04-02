077cc5cec71cf01cf00241f630a8a9d34051d39b
PedroMolon <pedroh.molonr@gmail.com>
2025-04-02 14:27:20 -0300

    Criação do UserDTO

---
+++ src/main/java/com/pedromolon/ToDo/DTO/UserDTO.java
@@ -0,0 +1,56 @@
+package com.pedromolon.ToDo.DTO;
+
+import com.pedromolon.ToDo.entity.Task;
+
+import java.util.List;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private List<Task> tasks;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String name, String email, List<Task> tasks) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.tasks = tasks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+}
